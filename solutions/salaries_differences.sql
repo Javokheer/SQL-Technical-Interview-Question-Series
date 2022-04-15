af8574efa5efe79a4f30efa33a5df7ab108457ab
Javokhir Khusanov <xjavohir005@gmail.com>
2022-04-15 16:31:48 +0500

    git commit -m "solution 🗝: Easy Dropbox ID 10308"

---
+++ solutions/salaries_differences.sql
@@ -0,0 +1,20 @@
+/*
+Salaries Differences
+
+Write a query that calculates the difference between the highest salaries found in the marketing and engineering departments.
+Output just the absolute difference in salaries.
+*/
+
+
+
+
+SELECT ABS(
+(SELECT MAX(emp.salary) 
+FROM db_employee emp
+JOIN db_dept dept ON emp.department_id = dept.id
+WHERE dept.department = 'engineering') 
+-
+(SELECT MAX(emp.salary) 
+FROM db_employee emp
+JOIN db_dept dept ON emp.department_id = dept.id
+WHERE dept.department = 'marketing')) 
