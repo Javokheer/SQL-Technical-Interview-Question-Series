eade8ecb904ad90724111015c7cc2d2123891398
Javokhir Khusanov <xjavohir005@gmail.com>
2022-04-15 19:13:22 +0500

    solution 🗝: Medium Meta/Facebook ID 10087

---
+++ solutions/posts_with_hear_reaction.sql
@@ -0,0 +1,13 @@
+/*
+Find all posts which were reacted to with a heart
+
+Find all posts which were reacted to with a heart.
+*/
+
+
+
+
+SELECT DISTINCT p.* 
+FROM facebook_posts p
+JOIN facebook_reactions r ON p.post_id = r.post_id
+WHERE r.reaction = 'heart'
