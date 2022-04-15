0f64018d454147a44c5ee160f713d555244d81b2
Javokhir Khusanov <xjavohir005@gmail.com>
2022-04-15 19:49:14 +0500

    solution 🗝: Easy City of San Francisco ID 9924

---
+++ solutions/libraries_not_provided_email.sql
@@ -0,0 +1,15 @@
+/*
+Find libraries who haven't provided the email address in circulation year 2016 but their notice preference definition is set to email
+
+Find libraries who haven't provided the email address in circulation year 2016 but their notice preference definition is set to email.
+Output the library code.
+*/
+
+
+
+
+SELECT distinct home_library_code 
+FROM library_usage
+WHERE  circulation_active_year = 2016 AND
+      notice_preference_definition = 'email' 
+      AND provided_email_address = False 
