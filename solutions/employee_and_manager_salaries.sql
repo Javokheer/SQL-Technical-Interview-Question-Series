8d3c186ccf1c83426a227eb91eb602dda42051b9
Javokhir Khusanov <xjavohir005@gmail.com>
2022-04-15 19:08:35 +0500

    solution 🗝: Medium Dropbox ID 9894

---
+++ solutions/employee_and_manager_salaries.sql
@@ -0,0 +1,14 @@
+/*
+Employee and Manager Salaries
+
+Find employees who are earning more than their managers. Output the employee's first name along with the corresponding salary.
+*/
+
+
+
+
+SELECT e.first_name,
+       e.salary
+FROM employee e
+JOIN employee m ON e.manager_id = m.id
+WHERE e.salary > m.salary
